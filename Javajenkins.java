4bbbb653f539443bfd4ff823ce3b1754ccf28ef3
My name <email@example.com>
2023-07-19 15:02:54 +0200

    premier commit

---
+++ Javajenkins.java
@@ -0,0 +1,6 @@
+public class Javajenkins {
+    public static void main(String[] args){System.out.println("Bonjour, World Devops !"); Additionner(1, 2); 
+    }
+     public static int Additionner(int a, int b){return(a + b);
+    }
+}
