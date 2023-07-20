a36fbbf4b9cfe305448d037fe0045cf82a9ef6df
My name <email@example.com>
2023-07-20 14:47:59 +0200

    2ème modification message

--- Javajenkins.java
+++ Javajenkins.java
@@ -1,5 +1,5 @@
 public class Javajenkins {
-    public static void main(String[] args){System.out.println("Bonjour, World Devops !!!"); Additionner(1, 2); 
+    public static void main(String[] args){System.out.println("Bonjour, World Devops !"); Additionner(1, 2); 
     }
      public static int Additionner(int a, int b){return(a + b);
     }
